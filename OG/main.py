0b8d93be83d5e634ba4429745219dd8539b62c78
samip6616 <samip2397@gmail.com>
2026-02-12 10:08:11 +0545

    Add new folder and contents

---
+++ OG/main.py
@@ -0,0 +1,22 @@
+''' Variable = A variable is a container for storing data values (string, integer, float, boolean, etc.)
+ A variable behaves as if it was the value
+'''
+#Strings
+first_name = "Bro"
+food = "pizza"
+email = "Bro123@fake.com"
+
+#print(first_name)
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
+
+#Integers
+age = 21
+quantity = 3
+num_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students")
