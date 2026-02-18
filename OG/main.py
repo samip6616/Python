ae745327df2cb7c7a7664df5e13775ab432475fe
samip6616 <samip2397@gmail.com>
2026-02-18 14:55:47 +0545

    Adding other variables

--- OG/main.py
+++ OG/main.py
@@ -20,3 +20,31 @@ num_of_students = 30
 print(f"You are {age} years old")
 print(f"You are buying {quantity} items")
 print(f"Your class has {num_of_students} students")
+
+# Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+print(f"The price is ${price}")
+print(f"Your GPA is {gpa}")
+print(f"You ran {distance} miles")
+
+#Boolean
+is_student = True
+for_sale = False
+is_online = True
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT student")
+
+if for_sale:
+    print("That item is for sale")
+else:
+    print("That item is NOT available")
+if is_online:
+    print("You are online")
+else:
+    print("You are NOT online")
