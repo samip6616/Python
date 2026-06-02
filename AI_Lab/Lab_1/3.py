e18bcf19bc9fbc1a2264f2e4a183996b4408568e
samip6616 <samip2397@gmail.com>
2026-06-02 20:33:53 +0545

    create variables
    Create variables of the following types and print their values and types:
    Integer
    Float
    String
    Boolean
    Complex number

---
+++ AI_Lab/Lab_1/3.py
@@ -0,0 +1,21 @@
+# Creating an integer variable
+integer_var = 10  # stores a whole number
+
+# Creating a float variable
+float_var = 3.14  # stores a decimal number
+
+# Creating a string variable
+string_var = "Hello, World!"  # stores text
+
+# Creating a boolean variable
+boolean_var = True  # stores True or False value
+
+# Creating a complex number variable
+complex_var = 2 + 3j  # stores a complex number (real + imaginary part)
+
+# Printing values and their types
+print("Integer value:", integer_var, "Type:", type(integer_var))
+print("Float value:", float_var, "Type:", type(float_var))
+print("String value:", string_var, "Type:", type(string_var))
+print("Boolean value:", boolean_var, "Type:", type(boolean_var))
+print("Complex value:", complex_var, "Type:", type(complex_var))
