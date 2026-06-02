df42cbda9208688b3b9c0f2022e0823d7ffd71a5
samip6616 <samip2397@gmail.com>
2026-06-02 20:36:32 +0545

    Add basic arithmetic operations in 5.py
    a program that takes two numbers and performs:
    Addition
    Subtraction
    Multiplication
    Division
    Floor division
    Modulus
    Exponentiation

---
+++ AI_Lab/Lab_1/5.py
@@ -0,0 +1,11 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Display the results
+print("Addition: ", num1 + num2)
+print("Subtraction: ", num1 - num2)
+print("Multiplication: ", num1 * num2)
+print("Division: ", num1 / num2)
+print("Floor Division: ", num1 // num2)
+print("Modulus: ", num1 % num2)
+print("Exponentiation: ", num1 ** num2)
