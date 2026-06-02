07f5348c959aa9b34f1585c1626cfa23736f2417
samip6616 <samip2397@gmail.com>
2026-06-02 20:32:42 +0545

    Add files via upload

---
+++ AI_Lab/Lab_1/2.py
@@ -0,0 +1,13 @@
+# Program to calculate the area of a rectangle
+
+# Take input from the user for the length of the rectangle
+length = float(input("Enter the length of the rectangle: "))
+
+# Take input from the user for the width of the rectangle
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate the area using the formula: area = length × width
+area = length * width
+
+# Display the calculated area to the user
+print("The area of the rectangle is:", area)
