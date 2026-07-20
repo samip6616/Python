629c4f39f0a7c257aa7ad51c726003ec95ce8fbd
samip6616 <samip2397@gmail.com>
2026-07-20 15:56:19 +0545

    Add Mathematical Operations on Student DataFrame
    This script performs mathematical operations and descriptive statistics on a DataFrame containing student data.

---
+++ OG/Pandas/DataFrame/Mathematical_Operations.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+student = {
+    "Name": ["Ram", "Hari", "Sita", "Gita", "Nikita", "Divine", "Sajan", "Kashmira"],
+    "Age": [20, 21, 19, 22, 27, 24, 23, 26],
+    "Marks": [85, 90, 95, 88, 86, 84, 83, 81]
+}
+
+df = pd.DataFrame(student)
+
+#MMathematical Operations
+print(df["Marks"].mean())
+print(df["Marks"].sum())
+print(df["Marks"].max())
+print(df["Marks"].min())
+
+#Descriptive Statistics
+print(df.describe())
